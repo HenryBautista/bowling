85077d1c65e477186bd087bbe18b5b28d7d0fe70
Henry Eloy Bautista Coaquira <henrybautistahtt@hotmail.com>
2023-05-29 22:24:42 -0400

    Add success roll test

--- src/Bowling.Tests/Bowling.Tests/Services/BowlingManagerServiceTests.cs
+++ src/Bowling.Tests/Bowling.Tests/Services/BowlingManagerServiceTests.cs
@@ -2,10 +2,10 @@ namespace Bowling.Tests.Services;
 
 using AutoFixture;
 using Bowling.Entities;
-using Bowling.Interfaces;
 using Bowling.App.Services;
 using FakeItEasy;
 using FluentAssertions;
+using Bowling.App.Services.Dtos;
 
 public class BowlingManagerServiceTests
 {
@@ -17,12 +17,67 @@ public class BowlingManagerServiceTests
 
     public BowlingManagerServiceTests()
     {
-        this.FrameService = A.Fake<FrameService>();
-        this.GameService = A.Fake<GameService>();
+        this.FrameService = A.Fake<IFrameService>();
+        this.GameService = A.Fake<IGameService>();
         this.Sut = new BowlingManagerService(
             this.FrameService,
             this.GameService);
         this.Fixture = new Fixture();
     }
 
+    [Fact]
+    public async Task Roll_Should_Return_RollResultDto_When_RollSaved_Message()
+    {
+        // Arrange
+        var gameId = 1;
+        var currentFrameNumber = 1;
+        char pinsDown = 'X';
+        
+        var game = new Game {
+            Id = gameId,
+            CurrentFrame = currentFrameNumber };
+        var frame = new Frame {
+            GameId = gameId,
+            FrameNumber = currentFrameNumber };
+        
+        A.CallTo(() => 
+            this.GameService.GetGameAsync(gameId)).Returns(game);
+        A.CallTo(() => 
+            this.GameService.UpdateGameAsync(A<Game>.Ignored));
+
+        A.CallTo(() => this.FrameService.RollIntoFrameAsync(
+            gameId,
+            currentFrameNumber,
+            pinsDown))
+        .Returns(frame);
+
+        // Act
+        var rollDto = new RollDto
+        {
+            GameId = gameId,
+            PinsDown = pinsDown
+        };
+
+        var result = await this.Sut.Roll(rollDto);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.GameId.Should().Be(gameId);
+        result.Message.Should().Be("Roll Saved!");
+
+        A.CallTo(() => 
+            this.GameService.GetGameAsync(gameId))
+        .MustHaveHappenedOnceExactly();
+
+        A.CallTo(() => 
+            this.GameService.UpdateGameAsync(game))
+        .MustHaveHappenedOnceExactly();
+
+        A.CallTo(() => 
+            this.FrameService.RollIntoFrameAsync(
+                gameId,
+                currentFrameNumber,
+                pinsDown))
+        .MustHaveHappenedOnceExactly();
+    }
 }
